f6d9870042182a39ecfff98ee6527e3a9e169ef0
rafalelo <rafal.kazimierski2@gmail.com>
2020-08-14 18:39:41 +0200

    Steps animation fixed

--- js/animations.js
+++ js/animations.js
@@ -1,21 +1,25 @@
 //step = id of step you are leaving
 function stepTransition(step){
-    $(`.step${step}`).find('h4').removeClass("active-step-title")
-    $(`.step${step}`).find('h4').addClass("inactive-step-title")
-    $(`.step${step}`).find('.step-icon').removeClass("active-icon")
-    $(`.step${step}`).find('.step-icon').addClass("inactive-icon")
-    $(`.step${step}`).find('.step-btn').removeClass("active-btn")
-    $(`.step${step}`).find('.step-btn').addClass("inactive-btn")
 
-    
+    if($(`.step${step}`).attr("done") === "false"){
+        $(`.step${step}`).find('h4').removeClass("active-step-title")
+        $(`.step${step}`).find('h4').addClass("inactive-step-title")
+        $(`.step${step}`).find('.step-icon').removeClass("active-icon")
+        $(`.step${step}`).find('.step-icon').addClass("inactive-icon")
+        $(`.step${step}`).find('.step-btn').removeClass("active-btn")
+        $(`.step${step}`).find('.step-btn').addClass("inactive-btn")
 
-    $(`.step${step + 1}`).find('h4').removeClass("inactive-step-title")
-    $(`.step${step + 1}`).find('h4').addClass("active-step-title")
-    $(`.step${step + 1}`).find('.step-icon').removeClass("inactive-icon")
-    $(`.step${step + 1}`).find('.step-icon').addClass("active-icon")
-    $(`.step${step + 1}`).find('.step-btn').removeClass("inactive-btn")
-    $(`.step${step + 1}`).find('.step-btn').addClass("active-btn")
-    $(`.step${step + 1}`).find('.step-btn').removeClass("disabled")
+        
+
+        $(`.step${step + 1}`).find('h4').removeClass("inactive-step-title")
+        $(`.step${step + 1}`).find('h4').addClass("active-step-title")
+        $(`.step${step + 1}`).find('.step-icon').removeClass("inactive-icon")
+        $(`.step${step + 1}`).find('.step-icon').addClass("active-icon")
+        $(`.step${step + 1}`).find('.step-btn').removeClass("inactive-btn")
+        $(`.step${step + 1}`).find('.step-btn').addClass("active-btn")
+        $(`.step${step + 1}`).find('.step-btn').removeClass("disabled")
+        $(`.step${step}`).attr("done", "true")
+    }
 }
 
 $('.flag').click(function(){
