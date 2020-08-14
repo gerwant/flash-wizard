e18e67e86e7ba1544944ae2daedee62084f0985a
rafalelo <rafal.kazimierski2@gmail.com>
2020-08-14 15:28:42 +0200

    Moved backend events to seperate file

--- index.js
+++ index.js
@@ -5,28 +5,21 @@ const updater = require("electron-updater");
 const autoUpdater = updater.autoUpdater;
 const unhandled = require('electron-unhandled');
 const debug = require('electron-debug');
-//const contextMenu = require('electron-context-menu');
 require('dotenv').config();
-const SerialPort = require('serialport');
 const i18n = require('./js/i18n');
-const spawn = require('child_process').spawn;
 const isDev = require('electron-is-dev');
-const FTPClient = require('ftp');
-const _ = require('underscore');
 
 
-var flash_config = {
-    port: null,
-    file_path: null,
-    baudrate: 57600,
-    processor: null
+
+
+class WindowManager{
+    constructor(){
+        this.mainWindow;
+        this.helpWindow;
+        this.isHelpOpen = false;
+    }
 }
-
-var hexpath_config = {
-    device: null,
-    sensor: null
-}
-
+let windowManager = new WindowManager
 
 unhandled();
 debug();
@@ -53,29 +46,6 @@ let isHelpOpen = false;
 
 let iconPath = (isDev)?'static/icon/wizzard.png': path.join(process.resourcesPath, "static/icon/wizzard.png")
 
-// FTP connection
-const client = new FTPClient();
-const excluded_dirs = ['.', '..']
-
-async function connectFTP(){
-
-    try {
-        client.connect({
-            host: process.env.FTP_HOST,
-            user: process.env.FTP_USER,
-            password: process.env.FTP_PASSWORD,
-            secure: false
-        })
-
-        return true
-        
-    } catch(err) {
-        // Should terminate window and return to welcome window
-        // with appropriate error message
-        console.log("FTP ERROR: ", err)
-        return false
-    }
-}
 
 /*
 
@@ -103,7 +73,7 @@ const createWelcomeWindow = async () => {
     win.on('closed', () => {
         // Dereference the window
         // For multiple windows store them in an array
-        mainWindow = undefined;
+        windowManager.mainWindow = undefined;
     });
 
 
@@ -129,14 +99,14 @@ const createHelpWindow = async () => {
 
     win.on('ready-to-show', () => {
         win.show();
-        isHelpOpen = true;
+        windowManager.isHelpOpen = true;
     });
 
     win.on('closed', () => {
         // Dereference the window
         // For multiple windows store them in an array
-        isHelpOpen = false;
-        helpWindow = null;
+        windowManager.isHelpOpen = false;
+        windowManager.helpWindow = null;
     });
 
     await win.loadFile(path.join(__dirname, 'help.html'));
@@ -159,12 +129,12 @@ if (!app.requestSingleInstanceLock()) {
 
 // Restore already opened window when somebody tries to open second app instance
 app.on('second-instance', () => {
-    if (mainWindow) {
-        if (mainWindow.isMinimized()) {
-            mainWindow.restore();
+    if (windowManager.mainWindow) {
+        if (windowManager.mainWindow.isMinimized()) {
+            windowManager.mainWindow.restore();
         }
 
-        mainWindow.show();
+        windowManager.mainWindow.show();
     }
 });
 
@@ -179,196 +149,24 @@ app.on('window-all-closed', () => {
 // For ex. when taskbar or dock icon clicked
 // or application's gonna be relaunched when it's running
 app.on('activate', async () => {
-    if (!mainWindow) {
-        mainWindow = await createWelcomeWindow();
+    if (!windowManager.mainWindow) {
+        windowManager.mainWindow = await createWelcomeWindow();
     }
 });
 
 // Create first welcome screen when app is ready.
 (async () => {
 	await app.whenReady();
-    mainWindow = await createWelcomeWindow();
+    windowManager.mainWindow = await createWelcomeWindow();
  
 })();
 
-/*
 
-Communication with frontend.
-Events defined to perform actions 
-basing on frontend signals.
 
-*/
+require('./js/backend-events')(ipcMain, windowManager, createHelpWindow)
 
-ipcMain.on('devices-list-request', async (event, arg) => {
-    if (!client){
-        console.error('No ftp connection')
-        return;
-    }
-    client.list('/', (err, listing) => {
-        console.log(listing)
-        listing = _.filter(listing, (element)=>{
-            return element.type == 'd' && !excluded_dirs.includes(element.name); // 2 because type 2 means it's a directory
-        })
-        listing = _.map(listing, (element) => {
-            return element.name;
-        })
-        console.log(listing)
-        event.sender.send('dropdown-content', {dropdown: "processors", content: listing})
-    })
-    
-})
 
-ipcMain.on('sensors-list-request', async (event, arg) => {
-    hexpath_config.device = arg.device;
-    client.listSafe(`/${hexpath_config.device}`, (err, listing) =>{
 
-        let procfile = _.find(listing, (el) => { return el.name.includes('.processor')}) // Look for .processor file
-
-        if (procfile){
-            flash_config.processor = procfile.name.split('.')[0]
-            console.log(flash_config)
-            let lst = _.filter(listing, (el) => { //Look for all directories meaning sensors
-                return el.type == 'd' && !excluded_dirs.includes(el.name);
-            })
-            lst = _.map(lst, (el) => { // Select only names of them
-                return el.name;
-            })
-            event.sender.send('dropdown-content', {dropdown: "sensors", content: lst})
-        } else { // No .processor file means something went wrong and flash can't be performed
-            event.sender.send('dropdown-content', {dropdown: "sensors", content: ''})
-        }
-        
-    })
-    
-})
-
-ipcMain.on('perform-flash', (event, arg) => {
-    const avrdude_exec = (process.platform === "win32") ? 'avrdude.exe' : 'avrdude'
-
-    let avrdude_path = ''
-    let avrdude_config_path = ''
-    if(isDev){
-        avrdude_path = path.join(__dirname, 'bin/')+avrdude_exec;
-        avrdude_config_path = path.join(__dirname, 'bin/')+'avrdude.conf'
-    } else {
-        avrdude_path = path.join(process.resourcesPath, "bin/")+avrdude_exec;
-        avrdude_config_path = path.join(process.resourcesPath, 'bin/')+'avrdude.conf' //process.resourcesPath
-    }
-    console.log(avrdude_path)
-    const avrdude_args = [
-        '-C'+avrdude_config_path,
-        '-p'+flash_config.processor,
-        '-carduino',
-        '-P'+flash_config.port,
-        '-b'+flash_config.baudrate,
-        '-D',
-        '-Uflash:w:'+flash_config.file_path+':i'
-    ]
-    let child = null;
-    if (process.platform === "win32"){
-        child = spawn('cmd.exe', ['/c', avrdude_path].concat(avrdude_args))
-    } else {
-        child = spawn(avrdude_path, avrdude_args);
-    }
-
-    child.stdout.on('data', (data) => {
-        let datastring = data.toString()
-        console.log('stdout: ', datastring);
-        if (datastring.includes('avrdude done')||datastring.includes('avrdude.exe done')){
-            event.sender.send('avrdude-done', datastring)
-        } else {
-            event.sender.send('avrdude-response', datastring)
-        }
-    })
-
-    child.stderr.on('data', (data) => {
-        let datastring = data.toString()
-        console.log('stderr: ',datastring);
-        if (datastring.includes('avrdude done')||datastring.includes('avrdude.exe done')){
-            event.sender.send('avrdude-done', datastring)
-        } else {
-            event.sender.send('avrdude-response', datastring)
-        }
-    })
-
-    child.on('error', (err) => {
-        console.log("ERROR DURING STARTUP", err);
-        event.sender.send('avrdude-done', null)
-    })
-})
-
-ipcMain.on('port-list-request', function (event, arg) {
-    function listPorts() {
-        let list_ports = []
-        SerialPort.list().then(
-         ports => {
-            
-             event.sender.send('port-list-reply', ports);
-            },
-         err => {
-          console.error('Error listing ports', err)
-         }
-        )
-       }
-       
-      listPorts()
-     
-    })
-     
-ipcMain.on('send-config-request', function (event, value, field) {
-    flash_config[field] = value
-})
-
-ipcMain.on('update-sensor', (event, data)=> {
-    hexpath_config.sensor = data.sensor;
-    console.log(hexpath_config)
-})
-
-ipcMain.on('change-language-request', function (event, atr) {
-    i18n.changeLanguage(atr)
-    mainWindow.reload()
-})
-
-ipcMain.on('openMainWindow', function (event, atr) {
-    (async () => {
-        mainWindow.loadFile('index.html')
-        //let welcome_window = mainWindow
-        //console.log(mainWindow)
-        //mainWindow = await createMainWindow();
-        //console.log(mainWindow)
-     
-    })();
-})
-
-ipcMain.on('openNohexWindow', function(e, atr) {
-    (async () => {
-        await connectFTP();
-        mainWindow.loadFile('nohex.html')
-    })();
-})
-
-ipcMain.on('openHelpWindow', function (event, atr) {
-    (async () => {
-        if (isHelpOpen){
-            return;
-        } else {
-            helpWindow = await createHelpWindow();     
-        }
-    })();
-})
-
-ipcMain.on('goToWelcome', function (event, atr) {
-    (async () => {
-        mainWindow.loadFile('welcome.html')
-    })();
-})
-
-ipcMain.on('close-help-window', function (event, atr) {
-    (async () => {
-        helpWindow.close()
-        isHelpOpen = false
-    })();
-})
 
 /*
 
---
+++ js/backend-events.js
@@ -0,0 +1,227 @@
+const SerialPort = require('serialport');
+const isDev = require('electron-is-dev')
+const i18n = require('./i18n')
+const FTPClient = require('ftp');
+const _ = require('underscore');
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+var flash_config = {
+    port: null,
+    file_path: null,
+    baudrate: 57600,
+    processor: null
+}
+
+var hexpath_config = {
+    device: null,
+    sensor: null
+}
+// FTP connection
+const client = new FTPClient();
+const excluded_dirs = ['.', '..']
+
+async function connectFTP(){
+    
+    try {
+        client.connect({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+            secure: false
+        })
+
+        return true
+        
+    } catch(err) {
+        // Should terminate window and return to welcome window
+        // with appropriate error message
+        console.log("FTP ERROR: ", err)
+        return false
+    }
+}
+
+
+module.exports = function(ipcMain, windowManager, createHelpWindow){
+    /*
+
+    Communication with frontend.
+    Events defined to perform actions 
+    basing on frontend signals.
+
+    */
+
+    ipcMain.on('change-language-request', function (event, atr) {
+        i18n.changeLanguage(atr)
+        windowManager.mainWindow.reload()
+    })
+    
+
+    ipcMain.on('port-list-request', function (event, arg) {
+        function listPorts() {
+            let list_ports = []
+            SerialPort.list().then(
+             ports => {
+                
+                 event.sender.send('port-list-reply', ports);
+                },
+             err => {
+              console.error('Error listing ports', err)
+             }
+            )
+           }
+           
+          listPorts()
+         
+        })
+    
+        
+    ipcMain.on('perform-flash', (event, arg) => {
+        const avrdude_exec = (process.platform === "win32") ? 'avrdude.exe' : 'avrdude'
+    
+        let avrdude_path = ''
+        let avrdude_config_path = ''
+        if(isDev){
+            avrdude_path = path.join(__dirname, 'bin/')+avrdude_exec;
+            avrdude_config_path = path.join(__dirname, 'bin/')+'avrdude.conf'
+        } else {
+            avrdude_path = path.join(process.resourcesPath, "bin/")+avrdude_exec;
+            avrdude_config_path = path.join(process.resourcesPath, 'bin/')+'avrdude.conf' //process.resourcesPath
+        }
+        console.log(avrdude_path)
+        const avrdude_args = [
+            '-C'+avrdude_config_path,
+            '-p'+flash_config.processor,
+            '-carduino',
+            '-P'+flash_config.port,
+            '-b'+flash_config.baudrate,
+            '-D',
+            '-Uflash:w:'+flash_config.file_path+':i'
+        ]
+        let child = null;
+        if (process.platform === "win32"){
+            child = spawn('cmd.exe', ['/c', avrdude_path].concat(avrdude_args))
+        } else {
+            child = spawn(avrdude_path, avrdude_args);
+        }
+    
+        child.stdout.on('data', (data) => {
+            let datastring = data.toString()
+            console.log('stdout: ', datastring);
+            if (datastring.includes('avrdude done')||datastring.includes('avrdude.exe done')){
+                event.sender.send('avrdude-done', datastring)
+            } else {
+                event.sender.send('avrdude-response', datastring)
+            }
+        })
+    
+        child.stderr.on('data', (data) => {
+            let datastring = data.toString()
+            console.log('stderr: ',datastring);
+            if (datastring.includes('avrdude done')||datastring.includes('avrdude.exe done')){
+                event.sender.send('avrdude-done', datastring)
+            } else {
+                event.sender.send('avrdude-response', datastring)
+            }
+        })
+    
+        child.on('error', (err) => {
+            console.log("ERROR DURING STARTUP", err);
+            event.sender.send('avrdude-done', null)
+        })
+    })
+
+    ipcMain.on('send-config-request', function (event, value, field) {
+        flash_config[field] = value
+    })
+    
+    ipcMain.on('update-sensor', (event, data)=> {
+        hexpath_config.sensor = data.sensor;
+        console.log(hexpath_config)
+    })
+
+    ipcMain.on('openMainWindow', function (event, atr) {
+        (async () => {
+            windowManager.mainWindow.loadFile('index.html')
+            //let welcome_window = mainWindow
+            //console.log(mainWindow)
+            //mainWindow = await createMainWindow();
+            //console.log(mainWindow)
+            
+        })();
+    })
+
+    ipcMain.on('openNohexWindow', function(e, atr) {
+        (async () => {
+            await connectFTP();
+            windowManager.mainWindow.loadFile('nohex.html')
+        })();
+    })
+    
+    ipcMain.on('openHelpWindow', function (event, atr) {
+        (async () => {
+            if (windowManager.isHelpOpen){
+                return;
+            } else {
+                windowManager.helpWindow = await createHelpWindow();     
+            }
+        })();
+    })
+
+    ipcMain.on('goToWelcome', function (event, atr) {
+        (async () => {
+            windowManager.mainWindow.loadFile('welcome.html')
+        })();
+    })
+
+    ipcMain.on('close-help-window', function (event, atr) {
+        (async () => {
+            windowManager.helpWindow.close()
+            windowManager.isHelpOpen = false
+        })();
+    })
+
+    ipcMain.on('devices-list-request', async (event, arg) => {
+        if (!client){
+            console.error('No ftp connection')
+            return;
+        }
+        client.list('/', (err, listing) => {
+            console.log(listing)
+            listing = _.filter(listing, (element)=>{
+                return element.type == 'd' && !excluded_dirs.includes(element.name); // 2 because type 2 means it's a directory
+            })
+            listing = _.map(listing, (element) => {
+                return element.name;
+            })
+            console.log(listing)
+            event.sender.send('dropdown-content', {dropdown: "processors", content: listing})
+        })
+        
+    })
+
+    ipcMain.on('sensors-list-request', async (event, arg) => {
+        hexpath_config.device = arg.device;
+        client.listSafe(`/${hexpath_config.device}`, (err, listing) =>{
+            let procfile = _.find(listing, (el) => { return el.name.includes('.processor')}) // Look for .processor file
+    
+        
+            if (procfile){
+                flash_config.processor = procfile.name.split('.')[0]
+                console.log(flash_config)
+                let lst = _.filter(listing, (el) => { //Look for all directories meaning sensors
+                    return el.type == 'd' && !excluded_dirs.includes(el.name);
+                })
+                lst = _.map(lst, (el) => { // Select only names of them
+                    return el.name;
+                })
+                event.sender.send('dropdown-content', {dropdown: "sensors", content: lst})
+            } else { // No .processor file means something went wrong and flash can't be performed
+                event.sender.send('dropdown-content', {dropdown: "sensors", content: ''})
+            }
+            
+        })
+        
+    })
+        
+}
