e185a6dfca6cfcec03cbf14b060f6b948487c8aa
Damian Gorzelańczyk <damian.gorzelanczyk@gmail.com>
2021-01-06 16:34:31 +0100

    :art: Semantic UI imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import ' cosemantic-ui-css/semantic.min.css'
+//import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import { render } from 'react-dom';
 import App from './Components/App';
+//import '../assets/semantic-ui-css/semantic.min.css'
 import './App.global.css';
 
 
