0cf1c909dad1db3e415c0b9c62d8f4f2f6773d04
Damian Gorzelańczyk <damian.gorzelanczyk@gmail.com>
2021-02-24 10:42:38 +0100

    :bug: Fixed bug with delayed loading of icons

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Titlebar, Color } from 'custom-electron-titlebar';
 import App from './Components/App';
-import 'semantic-ui-css/semantic.min.css';
+import '../assets/semantic-ui-css/semantic.min.css'
 import './style/App.global.css';
 
 const titlebar = new Titlebar({
