c9f3adfe72f70bf413a66ac6871b66e71d743aac
rafalelo <rafal.kazimierski2@gmail.com>
2021-03-09 21:16:43 +0100

    Update modal - part idk

--- src/Components/UpdateModal.tsx
+++ src/Components/UpdateModal.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
 const isDev = require('electron-is-dev');
-import {Modal, Button, Icon, TransitionablePortal, Accordion, Segment} from 'semantic-ui-react'
+import {Modal, Button, Icon, TransitionablePortal, Accordion, Segment, Dimmer, Loader} from 'semantic-ui-react'
 import electron from 'electron';
+import strings from '../localization';
 const UpdateModal = () => {
   const [open, setOpen] = React.useState(false)
+  const [downloading, setDownloading] = React.useState(false)
 
   useEffect(()=>{
 
@@ -20,21 +22,27 @@ const UpdateModal = () => {
 
   return (
     <div>
+
       <TransitionablePortal open={open}  transition={{ animation:'scale', duration: 300 }}>
         <Modal
           onClose={() => setOpen(false)}
           onOpen={() => setOpen(true)}
           open={open}
           style={{background: "#3C404B", height: "70%", width: "70%", marginTop: "5%"}}
+          closeOnDimmerClick={isDev}
+          closeOnEscape={isDev}
           >
-            <Icon
+        <Dimmer active={downloading}>
+          <Loader indeterminate style={{display: `${downloading? "": "none"}`}} size='large'>Downloading...</Loader>
+        </Dimmer>
+            {/* <Icon
               inverted
               size="big"
               name="close"
               className="close-modal pointer"
               style={{color: "white"}}
               onClick={()=>setOpen(false)}
-            />
+            /> */}
             <h2 className="downloadheader noselect">A new update has just arrived!</h2>
             <div className="ui segment container" >
 
@@ -42,9 +50,9 @@ const UpdateModal = () => {
 
                 <div className="footer" style={{marginLeft: "auto !important", marginRight: "auto !important"}}>
 
-                    <div onClick={()=>electron.ipcRenderer.send("download-update")} className="ui button active-btn download-update">Update</div>
+                    <div onClick={()=>{electron.ipcRenderer.send("download-update"); setDownloading(true)}} className="ui button active-btn download-update">{strings["Download now"]}</div>
 
-                    <div onClick={()=>setOpen(false)} style={{display: `${isDev?"": "none"}`}} className="ui button active-btn close-update-win" >Remind me later</div>
+                    {/* <div onClick={()=>setOpen(false)} style={{display: `${isDev?"block": "none"}`}} className="ui button active-btn close-update-win" >Remind me later</div> */}
 
                 </div>
             </div>
